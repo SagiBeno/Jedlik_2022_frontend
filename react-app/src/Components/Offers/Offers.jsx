da07097fcae00b1715705ed3672786fa330f1f96
SagiBeno <benosagi321@gmail.com>
2025-11-13 11:45:33 +0100

    Show static page

--- react-app/src/Components/Offers/Offers.jsx
+++ react-app/src/Components/Offers/Offers.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { Table } from 'react-bootstrap'
+import '../../bootstrap.min.css'
 
 export default class Offers extends React.Component {
     render() {
         return (
-            <>
-                <p>TODO</p>
-            </>
+            <div className='page-wrapper'>
+                <div className='container'>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+
+            </div>
         )
     }
 }

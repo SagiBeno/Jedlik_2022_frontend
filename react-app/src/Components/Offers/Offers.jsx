9f15449fd35d0e33a294a38fc6b127ca065d3e1b
SagiBeno <benosagi321@gmail.com>
2025-11-13 11:49:38 +0100

    Add columns

--- react-app/src/Components/Offers/Offers.jsx
+++ react-app/src/Components/Offers/Offers.jsx
@@ -6,21 +6,28 @@ export default class Offers extends React.Component {
     render() {
         return (
             <div className='page-wrapper'>
-                <div className='container'>
-                    <Table striped bordered hover>
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </div>
-
+                <h1 className='text-center'>Ajánlataink</h1>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Kategória</th>
+                            <th>Leírás</th>
+                            <th>Hirdetés<br />dátuma</th>
+                            <th>Tehermentes</th>
+                            <th>Fénykép</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Ház</td>
+                            <td>Leírás</td>
+                            <td>2025.11.13.</td>
+                            <td>Igen</td>
+                            <td><img src="https://almafa.png" alt="" /></td>
+                            <td>1</td>
+                        </tr>
+                    </tbody>
+                </Table>
             </div>
         )
     }

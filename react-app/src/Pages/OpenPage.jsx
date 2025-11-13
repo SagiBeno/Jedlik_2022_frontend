f8bbbc2769671e83d606c2a8a6ac5a350d4c421e
SagiBeno <benosagi321@gmail.com>
2025-11-13 09:09:27 +0100

    Remove import css, create OpenPage

---
+++ react-app/src/Pages/OpenPage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import '../bootstrap.min.css'
+import '../openpage.css'
+
+export default class OpenPage extends React.Component {
+    state = {}
+    
+    render() {
+        return (
+            <div className="container">
+                <div className="start w-100">
+                    <h1 className="text-center pt-2 pt-lg-4">Á.L.B. Ingatlanügynöség</h1>
+                    <div className="row">
+                        <div className="col-12 col-sm-6 text-center">
+                            <a className="btn btn-primary" href="#">Nézze meg kínálatunkat!</a>
+                        </div>
+                        <div className="col-12 col-sm-6 text-center">
+                            <a className="btn btn-primary" href="#">Hirdessen nálunk!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}

c6db6d900f8eb2a0532972eebee5b8b7fca10751
SagiBeno <benosagi321@gmail.com>
2025-11-13 11:41:05 +0100

    Make offers Link work

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import {Routes, Route} from 'react-router-dom'
 import './bootstrap.min.css'
 import OpenPage from './Components/OpenPage/OpenPage'
+import Offers from './Components/Offers/Offers'
 
 function App() {
   
@@ -8,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<OpenPage />} />
-      <Route path='/offers' element={<OpenPage />} />
+      <Route path='/offers' element={<Offers />} />
     </Routes>
   )
 }
--- react-app/src/Components/OpenPage/OpenPage.jsx
+++ react-app/src/Components/OpenPage/OpenPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './openpage.css'
 
 export default function OpenPage(props) {
@@ -9,10 +10,10 @@ export default function OpenPage(props) {
                     <h1 className="text-center pt-2 pt-lg-4">Á.L.B. Ingatlanügynöség</h1>
                     <div className="row">
                         <div className="col-12 col-sm-6 text-center">
-                            <a className="btn btn-primary" href="#">Nézze meg kínálatunkat!</a>
+                            <Link className="btn btn-primary" to='/offers'>Nézze meg kínálatunkat!</Link>
                         </div>
                         <div className="col-12 col-sm-6 text-center">
-                            <a className="btn btn-primary" href="#">Hirdessen nálunk!</a>
+                            <Link  className="btn btn-primary" to='/'>Hirdessen nálunk!</Link>
                         </div>
                     </div>
                 </div>

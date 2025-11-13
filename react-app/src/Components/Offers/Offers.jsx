da9fb3008e2d7fe70f999f38a8ec1ac96cd825ce
SagiBeno <benosagi321@gmail.com>
2025-11-13 14:05:43 +0100

    Modify css

--- react-app/src/Components/Offers/Offers.jsx
+++ react-app/src/Components/Offers/Offers.jsx
@@ -4,55 +4,57 @@ import '../../bootstrap.min.css'
 
 export default class Offers extends React.Component {
     state = {
-        tableData : []
+        tableData: []
     }
 
     componentDidMount() {
         fetch('http://localhost:3333/api/ingatlan')
-        .then((res) => res.json())
-        .then((tableData) => {
-            this.setState({tableData})
-        })
-        .catch(console.warn)
-        .finally( () => {} )
+            .then((res) => res.json())
+            .then((tableData) => {
+                this.setState({ tableData })
+            })
+            .catch(console.warn)
+            .finally(() => { })
     }
 
     render() {
         return (
             <div className='page-wrapper mx-auto'>
-                <h1 className='text-center'>Ajánlataink</h1>
-                <Table striped bordered hover responsive>
-                    <thead>
-                        <tr style={{verticalAlign: 'middle'}}>
-                            <th>Kategória</th>
-                            <th>Leírás</th>
-                            <th>Hirdetés<br />dátuma</th>
-                            <th>Tehermentes</th>
-                            <th>Fénykép</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.tableData.map( element => 
-                                <tr key={element.id} style={{verticalAlign: 'middle'}}>
-                                    <td>{element.kategoria}</td>
-                                    <td style={{textAlign: 'left'}}>{element.leiras}</td>
-                                    <td>{element.hirdetesDatuma}</td>
+                <div className='container' style={{backgroundColor: 'white'}}>
+                    <h1 className='text-center'>Ajánlataink</h1>
+                    <Table striped bordered hover responsive>
+                        <thead>
+                            <tr style={{ verticalAlign: 'middle', textAlign: 'center' }}>
+                                <th>Kategória</th>
+                                <th>Leírás</th>
+                                <th>Hirdetés<br />dátuma</th>
+                                <th>Tehermentes</th>
+                                <th>Fénykép</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.tableData.map(element =>
+                                    <tr key={element.id} style={{ verticalAlign: 'middle', textAlign: 'center' }}>
+                                        <td>{element.kategoria}</td>
+                                        <td style={{ textAlign: 'left' }}>{element.leiras}</td>
+                                        <td>{element.hirdetesDatuma}</td>
 
-                                    {
-                                        element.tehermentes
-                                        ?
-                                        <td style={{color: 'green'}}>Igen</td>
-                                        : 
-                                        <td style={{color: 'red'}}>Nem</td>
-                                    }
+                                        {
+                                            element.tehermentes
+                                                ?
+                                                <td style={{ color: 'green'}}>Igen</td>
+                                                :
+                                                <td style={{ color: 'red'}}>Nem</td>
+                                        }
 
-                                    <td><img src={element.kepUrl} alt="<property image placeholder>" width={200}/></td>
-                                </tr>
-                            )
-                        }
-                    </tbody>
-                </Table>
+                                        <td><img src={element.kepUrl} alt="<property image placeholder>" width={200} /></td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </Table>
+                </div>
             </div>
         )
     }
